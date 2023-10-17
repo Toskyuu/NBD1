6c6463ecfc2c436f18a1aed5bf4bca82ec7706c2
Toskyuu <robert.laski10@gmail.com>
2023-10-17 15:26:57 +0200

    add client tests

---
+++ src/main/java/tests/clientTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import mainClasses.Client;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class clientTest {
+
+    private final String f_name = "Robert";
+    private final String s_name = "Laski";
+    private final boolean is_archive = false;
+    private final String phoneNumber = "123123123";
+
+    private Client client0 = new Client();
+    @Test
+    void constructorTest() {
+        Client client1 = new Client(f_name, s_name, phoneNumber);
+        Assertions.assertEquals(client1.getFirstName(), f_name);
+        Assertions.assertEquals(client1.getSecondName(), s_name);
+        Assertions.assertEquals(client1.getPhoneNumber(), phoneNumber);
+    }
+
+    @Test
+    void setGetFirstNameTest() {
+        client0.setFirstName("Jaroslaw");
+        Assertions.assertEquals(client0.getFirstName(), "Jaroslaw");
+    }
+
+    @Test
+    void setGetSecondName() {
+        client0.setSecondName("Boberek");
+        Assertions.assertEquals(client0.getSecondName(), "Boberek");
+    }
+
+    @Test
+    void setGetPhoneNumber() {
+        client0.setPhoneNumber("112");
+        Assertions.assertEquals(client0.getPhoneNumber(), "112");
+    }
+
+
+}
