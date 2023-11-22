1eeac63fb2ec46fafaa1038db3b4389e40af7ee2
Pi8trz3k <pietrzak2001kacper@o2.pl>
2023-11-22 09:11:17 +0100

    Editing interface Repository

--- src/main/java/repositories/IRepository.java
+++ src/main/java/repositories/IRepository.java
@@ -1,46 +1,13 @@
 package repositories;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-public class IRepository<T> {
-    private T t;
-    private ArrayList<T> objects = new ArrayList<>();
-    public T getByID(int id){
-        if(id < 0){
-            //jakiś wyjątek rzucić :)
-        }
-        return objects.get(id);
-    }
-    public String getAll(){
-        String result = "";
-        for(T o : objects){
-            result += o.toString();
-        }
-        return result;
+public interface IRepository<T> {
 
-    }
-    public int getLastID(){
-        return objects.size()-1;
-    }
-    public void add(T object){
-        if(object == null){
-            //jakiś wyjątek rzucić :)
-        }
-        objects.add(object);
-    }
-
-    public void update(T newObject, int oldObjectID) {
-        if(newObject == null || oldObjectID < 0){
-            //jakiś wyjątek rzucić :)
-        }
-        objects.set(oldObjectID, newObject);
-    }
-
-    public void remove(T object) {
-        if (object == null) {
-            //jakiś wyjątek rzucić :)
-        }
-        objects.remove(object);
-    }
-
+    T FindById(UUID id);
+    void Add(T entity);
+    void Remove(T entity);
+    void Update(T entity);
+    List<T> FindAll();
 }
