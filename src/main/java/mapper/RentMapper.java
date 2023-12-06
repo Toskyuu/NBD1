b6541042c928bfa7dfd4efcaf399ab93f9186187
Pi8trz3k <pietrzak2001kacper@o2.pl>
2023-12-06 11:58:35 +0100

    Editing rent mapper class

--- src/main/java/mapper/RentMapper.java
+++ src/main/java/mapper/RentMapper.java
@@ -1,19 +1,19 @@
 package mapper;
 
-import mainClasses.Client;
-import mainClasses.Item;
 import mainClasses.Rent;
-import mgd.ClientMgd;
-import mgd.ItemMgd;
 import mgd.RentMgd;
+import org.bson.Document;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class RentMapper {
-//    private static final String ID = "_id";
-//    private static final String BEGIN_DATE = "begin_date";
-//    private static final String END_DATE = "end_date";
-//    private static final String RENT_COST = "rent_cost";
-//    private static final String CLIENT = "client";
-//    private static final String ITEM = "item";
+    private static final String ID = "_id";
+    private static final String BEGIN_DATE = "begin_date";
+    private static final String END_DATE = "end_date";
+    private static final String RENT_COST = "rent_cost";
+    private static final String CLIENT = "client";
+    private static final String ITEM = "item";
 
     public static RentMgd rentToMongo(Rent rent) {
 //        return new Document(ID, rent.getEntityID())
@@ -26,8 +26,19 @@ public class RentMapper {
 //        return null;
     }
 
-        public static Rent rentFromMongo(RentMgd rent) {
+    public static Rent rentFromMongo(RentMgd rent) {
             return new Rent(rent.getId(), rent.getBeginDate(), ClientMapper.clientFromMongo(rent.getClient()), ItemMapper.itemFromMongo(rent.getItem()));
+    }
+
+    public static RentMgd toRentMgd(Document rentDocument) {
+        return new RentMgd(
+                rentDocument.get(ID, Integer.class),
+                rentDocument.get(BEGIN_DATE, Date.class),
+                rentDocument.get(END_DATE, Date.class),
+                rentDocument.get(RENT_COST, Double.class),
+                ClientMapper.toClientMgd((Document) rentDocument.get(CLIENT)),
+                ItemMapper.toItemMgd((Document) rentDocument.get(ITEM))
+        );
     }
 
 //    public static Rent fromMongoDocument(Document document) {
