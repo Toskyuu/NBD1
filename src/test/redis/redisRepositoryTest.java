43e21f65fd777ebe61012b2a3db8ce8d6d9f8f3a
Pi8trz3k <pietrzak2001kacper@o2.pl>
2023-12-20 11:43:09 +0100

    Adding redis repository test

---
+++ src/test/redis/redisRepositoryTest.java
@@ -0,0 +1,72 @@
+package test.redis;
+
+import mainClasses.Client;
+import mainClasses.MusicAlbum;
+import mapper.ClientMapper;
+import mapper.ItemMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import redis.RentJson;
+import repositories.RedisRepositories.RentRedisRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class redisRepositoryTest {
+
+    RentRedisRepository rents = new RentRedisRepository();
+
+    @AfterEach
+    public void after() {
+        rents.clearCache();
+    }
+
+    @Test
+    public void addTest(){
+        Client client = new Client(1,"Janusz","Kowalski", false,"545656357");
+        MusicAlbum musicAlbum1 = new MusicAlbum(1,2001, 0,0, "album", "rap", "ktos", 24.56, 14);
+        MusicAlbum musicAlbum2 = new MusicAlbum(2,2021, 0,0, "album2", "pop", "ktos2", 29.56, 18);
+
+        RentJson rentJson = new RentJson(1, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum1));
+        RentJson rentJson2 = new RentJson(2, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum2));
+
+        assertTrue(rents.add(rentJson));
+        assertTrue(rents.add(rentJson2));
+
+        assertEquals(2, rents.findAll().size());
+    }
+
+    @Test
+    public void removeTest(){
+        Client client = new Client(1,"Janusz","Kowalski", false,"545656357");
+        MusicAlbum musicAlbum1 = new MusicAlbum(1,2001, 0,0, "album", "rap", "ktos", 24.56, 14);
+        MusicAlbum musicAlbum2 = new MusicAlbum(2,2021, 0,0, "album2", "pop", "ktos2", 29.56, 18);
+
+        RentJson rentJson = new RentJson(1, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum1));
+        RentJson rentJson2 = new RentJson(2, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum2));
+
+        assertTrue(rents.add(rentJson));
+        assertTrue(rents.add(rentJson2));
+
+        rents.remove(2);
+
+        assertEquals(1, rents.findAll().size());
+    }
+
+    @Test
+    public void clearCacheTest(){
+        Client client = new Client(1,"Janusz","Kowalski", false,"545656357");
+        MusicAlbum musicAlbum1 = new MusicAlbum(1,2001, 0,0, "album", "rap", "ktos", 24.56, 14);
+        MusicAlbum musicAlbum2 = new MusicAlbum(2,2021, 0,0, "album2", "pop", "ktos2", 29.56, 18);
+
+        RentJson rentJson = new RentJson(1, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum1));
+        RentJson rentJson2 = new RentJson(2, ClientMapper.toClientJson(client), ItemMapper.itemToRedis(musicAlbum2));
+
+        assertTrue(rents.add(rentJson));
+        assertTrue(rents.add(rentJson2));
+
+        rents.clearCache();
+
+        assertEquals(0, rents.findAll().size());
+    }
+
+}
