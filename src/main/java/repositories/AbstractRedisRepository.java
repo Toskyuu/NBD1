18f86151529e1872b4b34691bf7160e284de322d
Pi8trz3k <pietrzak2001kacper@o2.pl>
2023-12-20 11:41:04 +0100

    Adding redis repository classes

--- src/main/java/repositories/AbstractRedisRepository.java
+++ src/main/java/repositories/AbstractRedisRepository.java
@@ -1,5 +1,7 @@
 package repositories;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import redis.clients.jedis.DefaultJedisClientConfig;
@@ -7,18 +9,52 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPooled;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Set;
 
 public abstract class AbstractRedisRepository implements AutoCloseable {
     private static JedisPooled pool;
     private static Jsonb jsonb = JsonbBuilder.create();
+    private static String host;
+
+    static {
+        try {
+            host = getNode("host");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int port;
+
+    static {
+        try {
+            port = Integer.parseInt((getNode("port")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void initConnection() {
         JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
-        pool = new JedisPooled(new HostAndPort("localhost", 6379), clientConfig);
+        pool = new JedisPooled(new HostAndPort(host, port), clientConfig);
     }
 
     public AbstractRedisRepository() {
         initConnection();
+    }
+
+    private static String getNode(String node) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = new String(Files.readAllBytes(Paths.get("src/main/java/configuration/info.json")));
+        JsonNode jsonNode = objectMapper.readTree(json);
+        return jsonNode.get(node).asText();
+    }
+
+    private static String getHost() throws IOException {
+        return getNode("host");
     }
 
     public static JedisPooled getPool() {
@@ -41,5 +77,7 @@ public abstract class AbstractRedisRepository implements AutoCloseable {
         pool.getPool().destroy();
         pool.close();
     }
+
+
 }
 
---
+++ src/main/java/repositories/RedisRepositories/RentRedisRepository.java
@@ -0,0 +1,72 @@
+package repositories.RedisRepositories;
+
+import jakarta.json.bind.Jsonb;
+import mapper.RentMapper;
+import redis.RentJson;
+import redis.clients.jedis.JedisPooled;
+import repositories.AbstractRedisRepository;
+import repositories.IRepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class RentRedisRepository extends AbstractRedisRepository implements IRepository<RentJson> {
+    private JedisPooled pool = getPool();
+    private Jsonb jsonb = getJsonb();
+    private String prefix = "rent:";
+
+    @Override
+    public boolean add(RentJson entity) {
+        try {
+            String entityStr = jsonb.toJson(entity);
+            pool.set(prefix + entity.getId(), entityStr);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(int id) {
+        try {
+            pool.del(prefix + id);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(RentJson entity) {
+        try {
+            String entityStr = jsonb.toJson(entity);
+            pool.set(prefix + entity.getId(), entityStr);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public RentJson findById(int id) {
+        String found = pool.get(prefix + id);
+        return jsonb.fromJson(found, RentJson.class);
+    }
+
+    @Override
+    public List<RentJson> findAll() {
+        List<RentJson> all = new ArrayList<>();
+        Set<String> keys = pool.keys(prefix + "*");
+        for (String key : keys) {
+            all.add(jsonb.fromJson(pool.get(key), RentJson.class));
+        }
+
+        return all;
+    }
+
+}
